a47ee1dad0e0d7b785352f3e16688ebdd7b2179c
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-06-01 18:30:21 -0300

    Modificacion con Math.random Codigo modificado para que el metodo Math.random, me tire valores comprendidos entre dos parametros numerocos

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-let menoresDe21:number = 0;
-let mayoresDe21:number = 0;
-let personaEdad:number = 0;
-let indice:number = 1;
+let menoresDe21: number = 0;
+let mayoresDe21: number = 0;
+let personaEdad: number = 0;
+let indice: number = 1;
 
-function aleatorio(menorValor:number, mayorValor:number){
-  
+function aleatorio(menorValor: number, mayorValor: number) {
+  /*
   while (indice <= 270){
     personaEdad = Math.floor(Math.random()*100);
     if (personaEdad >= menorValor && personaEdad <=mayorValor){
@@ -21,5 +21,21 @@ function aleatorio(menorValor:number, mayorValor:number){
   console.log("La cantidad de personas mayores a 21 son "+ mayoresDe21);
   console.log("La cantidad de personas totales son "+ (indice-1));
 };
+*/
 
-aleatorio(19,40);
+  for (indice = 1; indice <= 270; indice++) {
+    personaEdad = Math.floor(
+      Math.random() * (mayorValor - menorValor) + menorValor
+    );
+    if (personaEdad < 21) {
+      menoresDe21++;
+    } else {
+      mayoresDe21++;
+    }
+  }
+  console.log("La cantidad de personas menores a 21 son " + menoresDe21);
+  console.log("La cantidad de personas mayores a 21 son " + mayoresDe21);
+  console.log("La cantidad de personas totales son " + (menoresDe21+mayoresDe21));
+};
+
+aleatorio(19, 40);
